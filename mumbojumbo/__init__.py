adf39f27567010457d57ad2a11c5fee79d2fce2a
Dmitry Duev <dima@wandb.com>
2022-10-31 16:32:39 -0700

    Bump version: 0.13.5.dev2 → 0.13.5.dev3

--- mumbojumbo/__init__.py
+++ mumbojumbo/__init__.py
@@ -1,3 +1,3 @@
 from .sdk.mumbo import Mumbo
 
-__version__ = "0.13.5.dev2"
+__version__ = "0.13.5.dev3"
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="mumbojumbo",
-    version="0.13.5.dev2",
+    version="0.13.5.dev3",
     packages=["mumbojumbo"],
     package_dir={"mumbojumbo": "mumbojumbo"},
 )
