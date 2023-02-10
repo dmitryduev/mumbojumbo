d115b902887fbc32967831bf6b3391005996f5e7
Dmitry Duev <dima@wandb.com>
2023-02-10 15:16:37 -0800

    feat(cz): add post bump hook

--- tools/cz_version_to_dev.sh
+++ tools/cz_version_to_dev.sh
@@ -1,2 +1,2 @@
-python3 -m bump2version patch --no-tag --no-commit
-python3 -m bump2version devkind --no-tag --no-commit
+python3.8 -m bump2version patch --no-tag --no-commit
+python3.8 -m bump2version devkind --no-tag --no-commit
