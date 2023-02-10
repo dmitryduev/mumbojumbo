d28bfcc7857c2370573aaa2d6180e9f647776fb7
Dmitry Duev <dima@wandb.com>
2023-02-10 15:15:21 -0800

    bump: version 0.13.15 → 0.13.16

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
+## v0.13.16 (2023-02-10)
+
+### :sparkles: Enhancements
+
+- **cz**: add post bump hook
+
 ## v0.13.15 (2023-02-10)
 
 ### :sparkles: Enhancements
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="mumbojumbo",
-    version="0.13.15",
+    version="0.13.16",
     packages=["mumbojumbo"],
     package_dir={"mumbojumbo": "mumbojumbo"},
 )
