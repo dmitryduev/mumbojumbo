5651d850fcffe5a3b5fc7c7b6c23cfb0933bedcc
Dmitry Duev <dima@wandb.com>
2023-02-10 14:17:54 -0800

    bump: version 0.13.7 → 0.13.8

---
+++ CHANGELOG.md
@@ -0,0 +1,13 @@
+## v0.13.8 (2023-02-10)
+
+### Feat
+
+- **sdk**: update LICENSE
+
+### Fix
+
+- **sdk**: update Mumbo class
+
+### Perf
+
+- **circus**: tigers and elephants
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="mumbojumbo",
-    version="0.13.7.dev1",
+    version="0.13.8.dev1",
     packages=["mumbojumbo"],
     package_dir={"mumbojumbo": "mumbojumbo"},
 )
