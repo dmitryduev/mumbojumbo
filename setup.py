ab8b3b3eb48a3d69ec8cf45d6a9e09d993312e7b
Katia <katia@wandb.com>
2023-02-11 11:28:05 -0800

    Bump version: 0.13.26 → 0.13.26.dev1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="mumbojumbo",
-    version="0.13.26",
+    version="0.13.26.dev1",
     packages=["mumbojumbo"],
     package_dir={"mumbojumbo": "mumbojumbo"},
 )
